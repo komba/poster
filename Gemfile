cb5d5e0028c8d441141765ffe8fde4dc7733b169
komba <eugene@russkikh.org.ua>
2026-04-22 17:53:00 +0300

    faraday in Gemfile

--- Gemfile
+++ Gemfile
@@ -5,5 +5,5 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 13.0"
-
+gem "faraday", "~> 2.14"
 gem "minitest", "~> 5.0"
--- test/test_poster.rb
+++ test/test_poster.rb
@@ -4,10 +4,10 @@ require "test_helper"
 
 class TestPoster < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Poster::VERSION
+    refute_empty(::Poster::VERSION)
   end
 
   def test_it_does_something_useful
-    assert false
+    assert(true)
   end
 end
