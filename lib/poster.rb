b8b458258b65b60d5c4fef04832143aaef4681aa
komba <eugene@russkikh.org.ua>
2026-04-22 17:45:38 +0300

    faraday

--- lib/poster.rb
+++ lib/poster.rb
@@ -1,83 +1,94 @@
+require "faraday"
 require "json"
-require "rest-client"
 
 module Poster
+  class Error < StandardError
+  end
+
   class Base
-    @poster_url = "https://joinposter.com/api"
-    @token = ENV["POSTER_TOKEN"]
+    BASE_URL = "https://joinposter.com/api"
 
     class << self
-      def transactions(params={})
-        get("dash.getTransactions", params)
+      def transactions(params = {})
+        request(:get, "dash.getTransactions", params)
       end
 
-      def delete_transaction(params={})
-        post("transactions.removeTransaction", params)
+      def delete_transaction(params = {})
+        request(:post, "transactions.removeTransaction", params)
       end
 
-      def products(params={})
-        get("menu.getProducts", params)
+      def products(params = {})
+        request(:get, "menu.getProducts", params)
       end
 
-      def update_product(params={})
-        post("menu.updateProduct", params)
+      def update_product(params = {})
+        request(:post, "menu.updateProduct", params)
       end
 
-      def online_order(params={})
-        post("incomingOrders.createIncomingOrder", params)
+      def online_order(params = {})
+        request(:post, "incomingOrders.createIncomingOrder", params)
       end
 
-      def create_check(params={})
-        post("incomingOrders.createIncomingOrder", params)
+      def create_check(params = {})
+        request(:post, "incomingOrders.createIncomingOrder", params)
       end
 
-      def update_customer(params={})
-        post("clients.updateClient", params)
+      def update_customer(params = {})
+        request(:post, "clients.updateClient", params)
       end
 
-      def customers(params={})
-        get("clients.getClients", params)
+      def customers(params = {})
+        request(:get, "clients.getClients", params)
       end
 
-      def spots(params={})
-        get("spots.getSpots", params)
+      def spots(params = {})
+        request(:get, "spots.getSpots", params)
       end
 
-      def categories(params={})
-        get("menu.getCategories", params)
+      def categories(params = {})
+        request(:get, "menu.getCategories", params)
       end
 
-      def get(action, params={})
-        json = JSON.parse(
-          RestClient.get(
-            "#{@poster_url}/#{action}",
-            params: params.merge(token: @token)
-          ).body
-        )
-        if json["error"]
-          raise StandardError.new(json["error"])
+      private
+
+      def connection
+        @connection ||= Faraday.new(url: BASE_URL) do |f|
+          f.request(:url_encoded)
+          f.adapter(Faraday.default_adapter)
+          f.options.timeout = 10
+          f.options.open_timeout = 5
         end
-        json["response"]
-      rescue => e
-        puts e.inspect
       end
 
-      def post(action, params={})
-        json = JSON.parse(
-          RestClient.post(
-            "#{@poster_url}/#{action}?token=#{@token}",
-            params.to_json,
-            {
-              "Content-Type" => "application/json"
-            }
-          ).body
-        )
-        if json["error"]
-          raise StandardError.new(json["error"])
+      def request(method, action, params = {})
+        query_params = {token: ENV["POSTER_TOKEN"]}
+
+        response = if method == :get
+          connection.get(action, query_params.merge(params))
+        else
+          connection.post("#{action}?#{URI.encode_www_form(query_params)}") do |req|
+            req.headers["Content-Type"] = "application/json"
+            req.body = params.to_json
+          end
         end
-        json["response"]
-      rescue => e
-        puts e.inspect
+
+        handle_response(response)
+      rescue Faraday::Error => e
+        raise Error, "Network error: #{e.message}"
+      end
+
+      def handle_response(response)
+        begin
+          body = JSON.parse(response.body)
+        rescue JSON::ParserError
+          raise Error, "Invalid JSON response: #{response.status}"
+        end
+
+        if body["error"]
+          raise Error, "Poster API Error #{body["error"]}: #{body["message"]}"
+        end
+
+        body["response"]
       end
     end
   end
